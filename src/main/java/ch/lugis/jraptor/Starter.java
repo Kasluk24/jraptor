fd132d2bc8c8d54031f3c578a376bcf3a8e7e07a
Lukas Gafner <62466873+Kasluk24@users.noreply.github.com>
2022-12-15 17:52:25 +0100

    Tests

--- src/main/java/ch/lugis/jraptor/Starter.java
+++ src/main/java/ch/lugis/jraptor/Starter.java
@@ -5,15 +5,17 @@ import java.io.IOException;
 import com.opencsv.exceptions.CsvValidationException;
 
 import ch.lugis.jraptor.gtfs.model.GtfsAgency;
+import ch.lugis.jraptor.gtfs.model.GtfsCalendar;
 
 public class Starter {
 
 	public static void main(String[] args) {
-		// Test GtfsReader agency
+		// Test GtfsReader
 		GtfsReader reader = new GtfsReader("data/gtfs_fp2022_2022-08-17_04-15");
 		
 		try {
 			reader.readAgenciesToMemory();
+			reader.readCalendarsToMemory();
 		} catch (CsvValidationException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -22,6 +24,9 @@ public class Starter {
 		for (GtfsAgency agency : reader.getGtfsAgencies()) {
 			System.out.println(agency.toString());
 		}
-		
+		for (GtfsCalendar calendar : reader.getGtfsCalendars()) {
+			System.out.println(calendar.toString());
+		}
+
 	}
 }
