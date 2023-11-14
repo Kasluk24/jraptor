d0b369405b4d361c65ae7ed8e071d608dd42a7b3
Lukas Gafner <62466873+Kasluk24@users.noreply.github.com>
2023-11-14 15:39:56 +0100

    Work on gtfsFeedReader

--- src/main/java/ch/weinetz/jraptor/GtfsFeedReader.java
+++ src/main/java/ch/weinetz/jraptor/GtfsFeedReader.java
@@ -1,5 +1,38 @@
 package ch.weinetz.jraptor;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import ch.weinetz.jraptor.gtfs.model.GtfsAgency;
+import ch.weinetz.jraptor.gtfs.model.GtfsFeed;
+import ch.weinetz.jraptor.gtfs.model.GtfsTableData;
+
 public class GtfsFeedReader  {
+	// Fields
+	private String gtfsFeedDirectory;
 	
+	// Constructor
+	public GtfsFeedReader(String gtfsFeedDirectory) {
+		this.gtfsFeedDirectory = gtfsFeedDirectory;		
+	}
+	
+	// Public methods
+	public GtfsFeed readFeed() {
+		GtfsFeed gtfsFeed = new GtfsFeed();
+		
+		//TODO:
+		
+		return gtfsFeed;
+		
+	}
+	
+	public GtfsFeed readFeedParallel() {
+		GtfsFeed gtfsFeed = new GtfsFeed();
+		
+		//TODO:
+		
+		return gtfsFeed;
+		
+	}
+		
 }
