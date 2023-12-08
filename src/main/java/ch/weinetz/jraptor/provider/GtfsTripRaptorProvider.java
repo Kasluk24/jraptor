f73f63ad498c0918e02f158cfe263caca68ef59b
Lukas Gafner <62466873+Kasluk24@users.noreply.github.com>
2023-12-08 14:32:50 +0100

    Trip Provider

--- src/main/java/ch/weinetz/jraptor/provider/GtfsTripRaptorProvider.java
+++ src/main/java/ch/weinetz/jraptor/provider/GtfsTripRaptorProvider.java
@@ -6,16 +6,22 @@ import java.util.Optional;
 import com.raoulvdberge.raptor.model.Trip;
 import com.raoulvdberge.raptor.provider.TripDetailsProvider;
 
+import ch.weinetz.jraptor.gtfs.model.GtfsFeed;
 import ch.weinetz.jraptor.gtfs.model.GtfsRoute;
 import ch.weinetz.jraptor.gtfs.model.GtfsStop;
+import ch.weinetz.jraptor.gtfs.model.GtfsTrip;
 
-public class GtfsTripRaptorProvider implements TripDetailsProvider<GtfsRoute, GtfsStop> {
-
+public class GtfsTripRaptorProvider implements TripDetailsProvider<GtfsTrip, GtfsStop> {
+	private final GtfsFeed feed;
+	
+	public GtfsTripRaptorProvider(GtfsFeed feed) {
+		this.feed = feed;
+	}		
+	
 	@Override
-	public Optional<Trip<GtfsStop>> getEarliestTripAtStop(GtfsRoute route, int stopIndex, LocalDateTime time) {
+	public Optional<Trip<GtfsStop>> getEarliestTripAtStop(GtfsTrip route, int stopIndex, LocalDateTime time) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
+	
 }
